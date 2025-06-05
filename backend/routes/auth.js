496592bc7b2a4864c555b07e208282db52ea63d9
moonwalkerfreaks <moonwalkerfreaks@gmail.com>
2025-06-05 09:52:08 +0000

    Removed an a duplicate line from auth.js in routes folder

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,6 @@ const { signupUser, loginUser } = require('../controllers/authController');
 
 router.post('/login', loginUser);
 router.post('/signup', signupUser);
-router.post('/login', loginUser);
 
 
 module.exports = router;
