7849f1a9e34108216c232cb1d1de89fb02ce4898
Murtaza Bhori <mbhori@madisonlogic.com>
2025-06-05 13:48:09 +0530

    Update public Ip in frontend/script.js

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
-  const res = await fetch('http://localhost:3000/api/login', {
+  const res = await fetch('http://3.110.132.46:3000/api/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password })
