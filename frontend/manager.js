eb1cff273985050817625ff0fd62ad44cab08a69
moonwalkerfreaks <moonwalkerfreaks@gmail.com>
2025-06-06 09:28:30 +0530

    rectifies ip address

--- frontend/manager.js
+++ frontend/manager.js
@@ -1,5 +1,5 @@
 async function fetchLeaveRequests() {
-  const res = await fetch('http://localhost:3000/api/leaves/pending');
+  const res = await fetch('http://3.110.132.46/:3000/api/leaves/pending');
   const data = await res.json();
 
   const container = document.getElementById('leave-requests');
@@ -26,7 +26,7 @@ async function fetchLeaveRequests() {
 }
 
 async function updateLeaveStatus(id, status) {
-  const res = await fetch(`http://http://3.110.132.46:3000/api/leaves/${id}`, {
+  const res = await fetch(`http://3.110.132.46:3000/api/leaves/${id}`, {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ status })
